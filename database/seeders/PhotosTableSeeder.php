3e1c45bc98651752cd4cd9a5112873a40c5dda82
BMWerke30 <weroniczka9@gmail.com>
2022-10-25 03:56:01 +0200

    poprawka3

--- database/seeders/PhotosTableSeeder.php
+++ database/seeders/PhotosTableSeeder.php
@@ -45,7 +45,7 @@ class PhotosTableSeeder extends Seeder
         DB::table('photos')->insert([
          'photoable_type' => 'App\User',
          'photoable_id' => $faker->unique()->numberBetween(1, 10),
-         'path' => $faker->imageUrl(200, 300, 'people'),
+         'path' => $faker->imageUrl(100, 200, 'people'),
 
           ]);
       }
