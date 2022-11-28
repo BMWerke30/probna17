0c2a64610629842612fa2629b70b856066953b18
BMWerke30 <weroniczka9@gmail.com>
2022-11-28 23:41:06 +0100

    t4version

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,11 +31,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <pre></pre>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
                     </ul>
 
                     <!-- Right Side Of Navbar -->
